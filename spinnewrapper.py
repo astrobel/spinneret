0d1d8487220ff389c62ceca23d131b93111ac932
Isabel <i.colman37@gmail.com>
2022-01-31 17:01:42 -0500

    Streamlining spinnewrapper pt. 2

--- spinnewrapper.py
+++ spinnewrapper.py
@@ -10,7 +10,7 @@ import os, sys, time, argparse
 from spinneret import *
 
 parser = argparse.ArgumentParser(description='Process a Kepler quarter and TESSified sector using spinneret')
-parser.add_argument('-i', '--id', required=True, type=int, help='KIC or TIC ID')
+parser.add_argument('-i', '--targetid', required=True, type=int, help='KIC or TIC ID')
 parser.add_argument('-s', '--rfset', required=False, default='test', type=str, choices=['test', 'train', 'validate'], help='test, train, or validate?')
 parser.add_argument('-r', '--rotating', required=False, default='yes', type=str, choices=['yes', 'no', 'unknown'], help='Is the star rotating?')
 parser.add_argument('-m', '--mission', required=False, default='TESS', type=str, choices=['TESS', 'TESSlike', 'Kepler'], help='TESS(like) sector or Kepler quarter?')
@@ -18,7 +18,7 @@ parser.add_argument('-n', '--number', required=True, type=int, help='TESS sector
 
 params = parser.parse_args()
 
-id = params.id
+tid = params.targetid
 forestset = params.rfset
 rotating = params.rotating
 mission = params.mission
@@ -59,104 +59,55 @@ elif forestset == 'validate':
     directorymaker('validatedata')
     targets = pd.read_csv(f'S21{rot_for_file}_test.csv')
 
-kic_r = targets['KIC']
-p_r = targets['Prot']
+p_r = targets['Prot'].loc[targets['KIC']==k] # will need changing when i have targets with TICs
 
-kep_cadence = 1/24/2 # 30min cadence
-tess_cadence = 1/24/30 # 2min cadence, for use later
+# start = time.time()
 
-nodata = [] # for stars with no data
+if len(str(k)) == 6: # this will also need a TIC version eventually
+    openstr = '000' + str(k)
+elif len(str(k)) == 7:
+    openstr = '00' + str(k)
+else:
+    openstr = '0' + str(k)
 
-# rotator sample
-for i, k in enumerate(kic_r):
+try:
+    hdu = fits.open(f'/data/shared_data/kepler/Q9/kplr{openstr}-2011177032512_llc.fits') # Q9, will need changing for TESS data and when i add other quarters
+except FileNotFoundError:
+    print(f'NO DATA: {k}')
+    nodata.append(k)
+    continue
 
-    # start = time.time()
-    if len(str(k)) == 6:
-        openstr = '000' + str(k)
-    elif len(str(k)) == 7:
-        openstr = '00' + str(k)
-    else:
-        openstr = '0' + str(k)
+table = hdu[1].data
+time = table['TIME']
+flux = table['PDCSAP_FLUX']
+hdu.close()
 
-    try:
-        hdu = fits.open(f'/data/shared_data/kepler/Q9/kplr{openstr}-2011177032512_llc.fits') # Q9, will need changing for TESS data
-    except FileNotFoundError:
-        print(f'NO DATA: {k}')
-        nodata.append(k)
-        continue
+if mission = 'TESSlike':
+    time, flux = tessify(time, flux)
 
-    table = hdu[1].data
-    time = table['TIME']
-    flux = table['PDCSAP_FLUX']
-    hdu.close()
+maxdays = 1/(time[-1] - time[0])
 
-    time, flux = nancleaner2d(time, flux)
-    time, flux = clip(time, flux, 3) #3 sigma clip
-    flux = lk.LightCurve(time=time, flux=flux).normalize().flux.value - 1
+time, flux = nancleaner2d(time, flux)
+time, flux = clip(time, flux, 3) #3 sigma clip
+flux = lk.LightCurve(time=time, flux=flux).normalize().flux.value - 1
 
-    target_kep = Spinner(time, flux)
+target_kep = Spinner(time, flux)
 
-    freq, ps = ts.LombScargle(time, flux).autopower(nyquist_factor=1, samples_per_peak=50, maximum_frequency=(1/90))
-    target_kep.ls_one_term(freq, ps)
+freq, ps = ts.LombScargle(time, flux).autopower(nyquist_factor=1, samples_per_peak=50, maximum_frequency=maxdays)
+target_kep.ls_one_term(freq, ps)
 
-    freq, ps = ts.LombScargle(time, flux, nterms=2).autopower(nyquist_factor=1, samples_per_peak=50, maximum_frequency=(1/90))
-    target_kep.ls_two_term(freq, ps)
+freq, ps = ts.LombScargle(time, flux, nterms=2).autopower(nyquist_factor=1, samples_per_peak=50, maximum_frequency=maxdays)
+target_kep.ls_two_term(freq, ps)
 
-    lags_raw, acf_raw, lags, acf, _x, _y = simple_acf(time, flux, kep_cadence, width=16)
-    target_kep.acf(lags, acf)
+lags_raw, acf_raw, lags, acf, _x, _y = simple_acf(time, flux, kep_cadence, width=16)
+target_kep.acf(lags, acf)
 
-    # fig1 = target_kep.diagnostic_plot(heading=f'KIC {k}: Kepler Q9 // Santos 21 period = {p_r[i]:.3f}d')
-    # figsaver(fig1, '/home/isy/Documents/Work/rotation/figs', f'KIC{k}_kep.png')
-    # figsaver(fig1, f'KIC{k}_kep.png')
-    filemaker(target_kep, k, p_r[i], filename=f'kic{k}_kepler.csv')
+# fig1 = target_kep.diagnostic_plot(heading=f'KIC {k}: Kepler Q9 // Santos 21 period = {p_r[i]:.3f}d')
+# figsaver(fig1, '/home/isy/Documents/Work/rotation/figs', f'KIC{k}_kep.png')
+# figsaver(fig1, f'KIC{k}_kep.png')
+filemaker(target_kep, k, p_r[i], filename=f'{id_prepend}{tid}_{file_append}.csv')
 
-    #####
+print(f'{tid} done')
 
-    time_tess, flux_tess = tessify(time, flux)
-
-    target_tess = Spinner(time_tess, flux_tess)
-
-    freq, ps = ts.LombScargle(time_tess, flux_tess).autopower(nyquist_factor=1, samples_per_peak=50, maximum_frequency=(1/27))
-    target_tess.ls_one_term(freq, ps)
-
-    freq, ps = ts.LombScargle(time_tess, flux_tess, nterms=2).autopower(nyquist_factor=1, samples_per_peak=50, maximum_frequency=(1/27))
-    target_tess.ls_two_term(freq, ps)
-
-    lags_raw, acf_raw, lags, acf, _x, _y = simple_acf(time_tess, flux_tess, kep_cadence, width=16)
-    target_tess.acf(lags, acf)
-
-    # fig2 = target_tess.diagnostic_plot(heading=f'KIC {k}: TESSify // Santos 21 period = {p_r[i]:.3f}d')
-    # figsaver(fig2, '/home/isy/Documents/Work/rotation/figs', f'KIC{k}_tess1.png')
-    # figsaver(fig2, f'KIC{k}_tess1.png')
-    filemaker(target_tess, k, p_r[i], filename=f'kic{k}_tess.csv')
-
-    #####
-
-    # time_butter, flux_butter = tessify(time, sps.sosfilt(sos, flux))
-
-    # target_butter = Spinner(time_butter, flux_butter)
-
-    # freq, ps = ts.LombScargle(time_butter, flux_butter).autopower(nyquist_factor=1, samples_per_peak=30)
-    # target_butter.ls_one_term(freq, ps)
-
-    # freq, ps = ts.LombScargle(time_butter, flux_butter, nterms=2).autopower(nyquist_factor=1, samples_per_peak=30)
-    # target_butter.ls_two_term(freq, ps)
-
-    # lags_raw, acf_raw, lags, acf, _x, _y = simple_acf(time_butter, flux_butter, kep_cadence, width=16)
-    # target_butter.acf(lags, acf)
-
-    # fig3 = target_butter.diagnostic_plot(heading=f'KIC {k}: TESSify + 27d Butterworth filter // Santos 21 period = {p_r[i]:.3f}d')
-    # # figsaver(fig3, '/home/isy/Documents/Work/rotation/figs', f'KIC{k}_tess2.png')
-    # figsaver(fig3, f'KIC{k}_tess2.png')
-    # filemaker(target_butter, k, p_r[i], filename=f'kic{k}_final.csv')
-
-    # if i == 3:
-    #     sys.exit()
-
-    print(f'{k} done')
-
-    # end = time.time()
-    # print(end-start)
-    # sys.exit()
-
-np.savetxt('noq9.dat', nodata, delimiter='\n')
+# end = time.time()
+# print(end-start)
